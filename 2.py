1b92ed11bc66aab672bc839f8f52a6fdd50b6bae
Maazin Kazi <maazinazim@gmail.com>
2025-01-22 22:11:12 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,30 @@
+def calculator():
+    print("Welcome to the Misbehaving Calculator!")
+    print("Choose operation: +, -, *, /")
+    operation = input("Enter operation: ")
+
+    try:
+        num1 = input("Enter first number: ")
+        num2 = input("Enter second number: ")
+    except:
+        print("Invalid input! Please enter numbers only.")
+        return
+
+    try:
+        if operation == "+":  
+            result = num1 + num2
+        elif operation == "-":
+            result = num1 - num2
+        elif operation == "*":
+            result = num1 * num2
+        elif operation == "/":
+            result = num1 / num2
+        else:
+            print("Invalid operation!")
+            return
+    except:
+        print("Oops.")
+        return
+    print("Result: " + result)
+
+calculator()
